dce46778830fa47d28fe0475ddc28f4c4874486c
benjaminsinaiko <ben.jamin@outlook.com>
2018-03-02 13:48:20 -0600

    First iteration passing all tests

--- hamming.rb
+++ hamming.rb
@@ -1,8 +1,10 @@
 class Hamming
   def self.compute(a, b)
-    hamming = 0
-    if a == b
-      return hamming
+    raise ArgumentError unless a.size == b.size
+    min = [a.size, b.size].min
+    a, b = a.chars, b.chars
+    (0...min).reduce(0) do |sum, index|
+      a[index] == b[index] ? sum : sum += 1
     end
   end
 end
@@ -10,5 +12,3 @@ end
 class BookKeeping
   VERSION=3
 end
-
-
